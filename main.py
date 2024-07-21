9eda5ad03542d7cf40e3a5b4cb7f7b722ca472df
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2024-07-21 21:03:32 +0700

    Add Progress bar

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from rich import print, pretty
 from rich.table import Table
 from rich.console import Console
 from rich import box
+from rich.progress import track,Progress, BarColumn, TextColumn
+from rich.table import Column
 from PIL import Image
 import requests
 
@@ -19,20 +21,29 @@ show_lines=True
 table.add_column(header='News',
                 justify="justified", style="white")
 console = Console()
+text_column = TextColumn("{task.description}", table_column=Column(ratio=1))
+bar_column = BarColumn(bar_width=None, table_column=Column(ratio=2))
+progress = Progress(text_column, bar_column, expand=True)
+
 configs = read_yaml("configs/configs.yaml")
 url_list = yaml_dot(configs,"news.urls")
-for url in url_list:
-    news_paper = newspaper.build(url,memoize_articles=False)
-    for article in news_paper.articles[0:yaml_dot(configs,"number_of_news")]:
-        article.download()
-        article.parse()
-        image = url_to_imagebit(article.top_image)
-        if yaml_dot(configs,"use_images"):
-            try:
-                added_text = f"[magneta]{article.publish_date}[/magneta] \n [green]{article.title}[/green] \n [blue]{article.url}[/blue] \n {imagebit_to_string(image,150)} \n"
-            except:
+with progress:
+    task_ = progress.add_task("[red]Getting news...[/red]",total=len(url_list)*yaml_dot(configs,"number_of_news"))
+    for url in url_list:
+        news_paper = newspaper.build(url,memoize_articles=False)
+        for article in news_paper.articles[0:yaml_dot(configs,"number_of_news")]:
+            article.download()
+            article.parse()
+            if yaml_dot(configs,"use_images"):
+                image = url_to_imagebit(article.top_image)
+                try:
+                    added_text = f"[magneta]{article.publish_date}[/magneta] \n [green]{article.title}[/green] \n [blue]{article.url}[/blue] \n {imagebit_to_string(image,150)} \n"
+                    progress.update(task_,description=f"{url} \n {article.title}",advance=1)
+                except:
+                    added_text = f"[magneta]{article.publish_date}[/magneta] \n [green]{article.title}[/green] \n [blue]{article.url}[/blue] \n "
+                    progress.update(task_,description=f"{url} \n {article.title}",advance=1)
+            else: 
                 added_text = f"[magneta]{article.publish_date}[/magneta] \n [green]{article.title}[/green] \n [blue]{article.url}[/blue] \n "
-        else: 
-            added_text = f"[magneta]{article.publish_date}[/magneta] \n [green]{article.title}[/green] \n [blue]{article.url}[/blue] \n "
-        table.add_row(added_text + article.text)
-console.print(table)
+                progress.update(task_,description=f"{url} \n {article.title}",advance=1)
+            table.add_row(added_text + article.text)
+    console.print(table)
