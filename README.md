201fc09bddcaaefc99c9c236aa8c5541d3385ad6
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2024-08-07 00:50:53 +0700

    Major Updates

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-![news_terminal](https://github.com/user-attachments/assets/184b492c-4f8a-4402-b0a6-09148ed3fa33)
+# Simple news aggregator in Python
+
+Sample `config.yml` file
+```
+news:
+  urls:
+    - https://www.coindesk.com/
+  block_list : 
+    - "#"
+    - "comment"
+number_of_news: 10
+use_images: True
+memoize_articles: False
+```
+Parameters:
+- `news.urls`: List of urls from which you want to get news from
+- `block_list`: List of keywords that you want to exclude in your news search
+- `number_of_news`: Number of news per url
+- `memoirize_articles`: Set to `True` if you want the news not to get duplicated each script run.  
+
+
+![GIF](./asset/screen_recording.gif)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from rich.table import Table, Column
 from rich.console import Console
 from rich.progress import Progress, BarColumn, TextColumn
 import re
+import argparse
 
 pretty.install()
 console = Console()
@@ -23,13 +24,32 @@ progress = Progress(text_column, bar_column, transient=True, expand=True, auto_r
 configs = read_yaml("configs/configs.yaml")
 url_list = yaml_dot(configs,"news.urls")
 
+description = """
+Simple news aggregator right in your terminal !
+"""
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--url", "-u", help="additional news url")
+    parser.add_argument("--block_list","-b", help="list of keywords to exclude")
+    parser.add_argument("--numb_news","-n", help="number of news per url")
+    parser.add_argument("--image","-im",action='store_true', help="Set this flag to show image")
+    parser.add_argument("--memoize","-me", help="Set to False for unduplicated news")
+    args = parser.parse_args()
+    number_of_news = args.numb_news if args.numb_news \
+        else yaml_dot(configs,"number_of_news")
+    memoize_articles =  args.memoize if args.memoize \
+        else yaml_dot(configs,"memoize_articles")
+    if args.url:
+        url_list.append(args.url)
     with progress:
-        task_ = progress.add_task("[red]Getting news...[/red]",total=len(url_list)*yaml_dot(configs,"number_of_news"))
+        task_ = progress.add_task("[red]Getting news...[/red]",
+                        total=len(url_list)*number_of_news)
         for url in url_list:
-            news_paper = newspaper.build(url,memoize_articles=yaml_dot(configs,"memoize_articles"))
-            # url_list = []
+            news_paper = newspaper.build(url,
+                        memoize_articles=memoize_articles)
             block_list = yaml_dot(configs,"news.block_list")
+            if args.block_list:
+                block_list.append(args.block_list)
             for article in news_paper.articles:
                 for block_item in block_list:
                     if re.search(block_item,article.url) :
@@ -39,25 +59,34 @@ if __name__ == "__main__":
                             pass
                     else:
                         continue
-            for article in news_paper.articles[0:yaml_dot(configs,"number_of_news")]:
+            for article in news_paper.articles[0:number_of_news]:
                 try:
                     article.download()
                     article.parse()
                 except Exception as e:
                     continue
-                if yaml_dot(configs,"use_images"):
+                use_images = args.image if args.image \
+                    else yaml_dot(configs,"use_images")
+                if use_images:
                     try:
                         image = url_to_imagebit(article.top_image)
-                        added_text = f"{article.publish_date} \n [green bold]{article.title}[/green bold] \n [blue]{article.url}[/blue] \n{imagebit_to_string(image,100)} \n \n"
-                        progress.update(task_,description=f"[blue]{url}[/blue] \n {article.title} \n\n{imagebit_to_string(image,70)}", advance=1)
+                        added_text = f"""
+                        [green bold]{article.title}\n[/green bold]\n{imagebit_to_string(image,100)}\n
+                        [blue]{article.url}[/blue] \n\n"""
+                        progress.update(task_,description=f"""[blue]{url}[/blue]\n{article.title}\n\n{imagebit_to_string(image,70)}""",advance=1)
                         progress.refresh()
                     except:
-                        added_text = f"{article.publish_date} \n [green bold]{article.title}[/green bold] \n [blue]{article.url}[/blue] \n \n "
-                        progress.update(task_,description=f"[blue]{url}[/blue] \n {article.title}", advance=1)
+                        added_text = f"""{article.publish_date}\n
+                        [green bold]{article.title}[/green bold]\n 
+                        [blue]{article.url}[/blue]\n\n"""
+                        progress.update(task_,description=f"""[blue]{url}[/blue]\n {article.title}""", advance=1)
                         progress.refresh()
-                else: 
-                    added_text = f"{article.publish_date} \n [green bold]{article.title}[/green bold] \n [blue]{article.url}[/blue] \n \n "
-                    progress.update(task_,description=f"[blue]{url}[/blue] \n {article.title}", advance=1)
+                else:
+                    added_text = f"""{article.publish_date}\n 
+                    [green bold]{article.title}[/green bold]\n
+                    [blue]{article.url}[/blue]\n\n """
+                    progress.update(task_,description=f"""[blue]{url}[/blue]\n
+                                {article.title}""", advance=1)
                     progress.refresh()
                 table.add_row(added_text + article.text)
     console.print(table)
